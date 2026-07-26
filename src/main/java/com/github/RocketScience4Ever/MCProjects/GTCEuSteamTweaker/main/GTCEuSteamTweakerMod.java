1ee5badc340b35afd2d0f2ab238cbef08f361745
RocketScience4Ever <aidan.chirico@gmail.com>
2026-07-26 15:19:36 -0400

    Fix config for minermBPt requiring world restart or replacing the block. Als fixes the tooltip for the steam miner to reflect the value set by GTCEu Steam Tweaker.

--- src/main/java/com/github/RocketScience4Ever/MCProjects/GTCEuSteamTweaker/main/GTCEuSteamTweakerMod.java
+++ src/main/java/com/github/RocketScience4Ever/MCProjects/GTCEuSteamTweaker/main/GTCEuSteamTweakerMod.java
@@ -1,14 +1,8 @@
 package com.github.RocketScience4Ever.MCProjects.GTCEuSteamTweaker.main;
 
-import gregtech.common.metatileentities.steam.SteamMiner;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 /**Mod class. Right now only exists to make Forge recognize this mod as a mod
  */
 @Mod(modid = "gtceusteamtweaker", name = "GTCEu Steam Tweaker", version = "0.1.0", dependencies = "required:gregtech@[2.8.10-beta,)")
-public class GTCEuSteamTweakerMod {
-    public GTCEuSteamTweakerMod() {
-        MinecraftForge.EVENT_BUS.register(new SteamMiner(null,1,1,0));
-    }
-}
+public class GTCEuSteamTweakerMod { }
